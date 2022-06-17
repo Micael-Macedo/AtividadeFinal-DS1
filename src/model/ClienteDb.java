f0871c19cc6d919385af90805cd3e4e489d8d4b3
Micael Macedo <micaelm2009@hotmail.com>
2022-06-17 18:15:26 -0300

    feat: add sql Connection

--- src/model/ClienteDb.java
+++ src/model/ClienteDb.java
@@ -4,10 +4,38 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+
+
 /**
  *
  * @author micae
  */
 public class ClienteDb {
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
     
+    public boolean connect(){
+        String server = "jdbc:mysql://localhost:3306/projetodeal";
+        String user = "root";
+        String pwd = "1234";
+        String driver = "com.mysql.cj.jdbc.Driver";
+        
+        try{
+            Class.forName(driver);
+            this.connection = (Connection)DriverManager.getConnection(server, user, pwd);
+            this.statement = (Statement)this.connection.createStatement();
+        }catch(Exception e){
+            System.out.println("Erro ao conectar ao banco: "+e);
+        }
+        
+        if(this.connection !=  null){
+            return true;
+        }
+        return false;
+    }
 }
